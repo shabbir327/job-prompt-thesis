ef6afcfd02c821dda3b68955a990af80c235f4a9
Shabbir Haque Akash <32530627+shabbir327@users.noreply.github.com>
2026-03-16 10:21:37 +0100

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,1 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-export const SUPABASE_URL = "https://vjwcpzprgqzbjmwjrfrc.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqd2NwenByZ3F6Ymptd2pyZnJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2NTA5MTUsImV4cCI6MjA4ODIyNjkxNX0.vHt6h1o4kMmiQuFSPiPirqk4CR-E5gJiIb0HxCeT8g4";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
